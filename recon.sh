bbcdcaa5c98f6ebc4e1a9b2582205ac1b734fd3e
Jordan Swebeck <111098407+gruntledturtle@users.noreply.github.com>
2023-02-17 11:00:33 +1000

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-#requires hakrawler, gospider
+#requires gospider
 
 read -p "domain: " domain
 url="https://$domain"
@@ -16,10 +16,12 @@ do
      whois -h whois.cymru.com $ip >> whois.txt
 done<ip.txt
 printf "Getting subdomains"
-gobuster dns -d $domain -w /usr/share/wordlists/amass/subdomains-top1mil-5000.txt > subdomains.txt
+subfinder -d $domain > temp.txt
+assetfinder -subs-only $domain >> temp.txt
+sort subs.txt | uniq > subs.txt
+rm temp.txt 
+#gobuster dns -d $domain -w /usr/share/wordlists/amass/subdomains-top1mil-5000.txt > subdomains.txt
 printf "Spidering"
 gospider -s $url > spider.txt
-printf "Hakrawler"
-echo $domain | hakrawler > hakrawler.txt
 printf "Initial port scan"
 nmap -sC -sV -iL ip.txt -oN initial_nmap.txt
